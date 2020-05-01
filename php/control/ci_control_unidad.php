d1df8b2b3269ff41289c69902d92d38b9849cea2
Andrea <Andrea@AndreaGran>
2020-05-01 09:22:47 -0300

    descomento el grafico

--- php/control/ci_control_unidad.php
+++ php/control/ci_control_unidad.php
@@ -52,52 +52,52 @@ class ci_control_unidad extends toba_ci {
     //---- grafico ----------------------------------------------------------------------
     //-----------------------------------------------------------------------------------
 
-//    function conf__grafico(toba_ei_grafico $grafico) {
-//        if (isset($this->datos)) {
-//            $datos = array();
-//            $leyendas = array();
-//            foreach ($this->datos as $value) {
-//                $datos[] = $value['resultado'];
-//                $leyendas[] = $value['codc_uacad'];
-//            }
-//        }
-//        
-//            require_once (toba_dir() . '/php/3ros/jpgraph/jpgraph.php');
-//		require_once (toba_dir() . '/php/3ros/jpgraph/jpgraph_bar.php');
-//
-//
-//
-//
-//		// Setup a basic graph context with some generous margins to be able
-//		// to fit the legend
-//		$canvas = new Graph(900, 300);
-//		$canvas->SetMargin(100,140,60,40);
-//
-//		$canvas->title->Set('Cr�dito Disponible');
-//		//$canvas->title->SetFont(FF_ARIAL,FS_BOLD,14);
-//
-//		// For contour plots it is custom to use a box style ofr the axis
-//		$canvas->legend->SetPos(0.05,0.5,'right','center');
-//		$canvas->SetScale('intint');
-//		//$canvas->SetAxisStyle(AXSTYLE_BOXOUT);
-//		//$canvas->xgrid->Show();
-//		$canvas->ygrid->Show();
-//                $canvas->xaxis->SetTickLabels($leyendas);
-//
-//
-//		// A simple contour plot with default arguments (e.g. 10 isobar lines)
-//		$cp = new BarPlot($datos);
-//                $cp->SetColor("#B0C4DE");
-//                $cp->SetFillColor("#B0C4DE");
-//                $cp->SetLegend("Resultado");
-//
-//		$canvas->Add($cp);
-//
-//		// Con esta llamada informamos al gr�fico cu�l es el gr�fico que se tiene
-//		// que dibujar
-//		$grafico->conf()->canvas__set($canvas);
-//
-//    }
+    function conf__grafico(toba_ei_grafico $grafico) {
+        if (isset($this->datos)) {
+            $datos = array();
+            $leyendas = array();
+            foreach ($this->datos as $value) {
+                $datos[] = $value['resultado'];
+                $leyendas[] = $value['codc_uacad'];
+            }
+        }
+        
+            require_once (toba_dir() . '/php/3ros/jpgraph/jpgraph.php');
+		require_once (toba_dir() . '/php/3ros/jpgraph/jpgraph_bar.php');
+
+
+
+
+		// Setup a basic graph context with some generous margins to be able
+		// to fit the legend
+		$canvas = new Graph(900, 300);
+		$canvas->SetMargin(100,140,60,40);
+
+		$canvas->title->Set('Cr�dito Disponible');
+		//$canvas->title->SetFont(FF_ARIAL,FS_BOLD,14);
+
+		// For contour plots it is custom to use a box style ofr the axis
+		$canvas->legend->SetPos(0.05,0.5,'right','center');
+		$canvas->SetScale('intint');
+		//$canvas->SetAxisStyle(AXSTYLE_BOXOUT);
+		//$canvas->xgrid->Show();
+		$canvas->ygrid->Show();
+                $canvas->xaxis->SetTickLabels($leyendas);
+
+
+		// A simple contour plot with default arguments (e.g. 10 isobar lines)
+		$cp = new BarPlot($datos);
+                $cp->SetColor("#B0C4DE");
+                $cp->SetFillColor("#B0C4DE");
+                $cp->SetLegend("Resultado");
+
+		$canvas->Add($cp);
+
+		// Con esta llamada informamos al gr�fico cu�l es el gr�fico que se tiene
+		// que dibujar
+		$grafico->conf()->canvas__set($canvas);
+
+    }
 
 }
 
